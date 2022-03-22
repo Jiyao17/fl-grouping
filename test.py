e6af7a885a00f7baae91981ff3315d075eb5369f
Jiyao17 <jiyao.liu@outlook.com>
2022-03-22 15:27:29 -0400

    exp data

--- test.py
+++ test.py
@@ -37,14 +37,14 @@ if __name__ == "__main__":
     )
 
     fedavg = GFLConfig(
-        client_num = 500, lr=0.1, lr_interval=500,
+        client_num = 500, lr=1, lr_interval=150,
         data_num_per_client = 50, local_batch_size = 50,
-        global_epoch_num= 200, reselect_interval=3000,
+        global_epoch_num= 300, reselect_interval=3000,
 
         server_num = 1,
         group_epoch_num=1, local_epoch_num=5, r = 2, 
         l = 60, max_delay = 60, max_connection = 5000,
-        log_interval=20,
+        log_interval=150,
 
         grouping_mode='no',
         regroup_size=1, # 1: no regrouping,
@@ -110,16 +110,16 @@ if __name__ == "__main__":
     )
 
     grouping = GFLConfig(
-        client_num = 500, lr=1, lr_interval=50,
+        client_num = 500, lr=0.5, lr_interval=5,
         data_num_per_client = 50, local_batch_size = 50,
-        global_epoch_num= 100, reselect_interval=3000,
+        global_epoch_num= 10, reselect_interval=3000,
 
-        server_num = 1,
-        group_epoch_num=10, local_epoch_num=1, r = 2, 
-        l = 60, max_delay = 60, max_connection = 500, 
-        log_interval=10,
+        server_num=1,
+        group_epoch_num=30, local_epoch_num=5, r = 2, 
+        l = 60, max_delay = 60, max_connection = 5000, 
+        log_interval=1,
 
-        grouping_mode='noiid',
+        grouping_mode='iid',
         regroup_size=10, # 1: no regrouping,
         group_size=10, # 1: no grouping,
         comment="single server noniid grouping", 
@@ -129,12 +129,12 @@ if __name__ == "__main__":
     )
 
     grouping_noiid = GFLConfig(
-        client_num = 500, lr=0.1, lr_interval=5,
+        client_num = 500, lr=1, lr_interval=5,
         data_num_per_client=50, local_batch_size = 50,
         global_epoch_num=10, reselect_interval=3000,
 
         server_num = 1,
-        group_epoch_num=100, local_epoch_num=1, r = 2, 
+        group_epoch_num=30, local_epoch_num=5, r = 2, 
         l = 60, max_delay = 60, max_connection = 5000, 
         log_interval=1,
 
@@ -148,12 +148,12 @@ if __name__ == "__main__":
     )
 
     grouping_iid = GFLConfig(
-        client_num = 500, lr=0.1, lr_interval=5,
+        client_num = 500, lr=1, lr_interval=5,
         data_num_per_client = 50, local_batch_size = 50,
         global_epoch_num= 10, reselect_interval=3000,
 
         server_num=1,
-        group_epoch_num=100, local_epoch_num=1, r = 2, 
+        group_epoch_num=30, local_epoch_num=5, r = 2, 
         l = 60, max_delay = 60, max_connection = 5000, 
         log_interval=1,
 
@@ -167,12 +167,12 @@ if __name__ == "__main__":
     )
 
     grouping_random = GFLConfig(
-        client_num = 500, lr=0.1, lr_interval=5,
+        client_num = 500, lr=1, lr_interval=5,
         data_num_per_client = 50, local_batch_size = 50,
-        global_epoch_num= 10, reselect_interval=3000,
+        global_epoch_num=10, reselect_interval=3000,
 
         server_num = 1,
-        group_epoch_num=100, local_epoch_num=1, r = 2, 
+        group_epoch_num=30, local_epoch_num=5, r = 2, 
         l = 60, max_delay = 60, max_connection = 5000, 
         log_interval=1,
 
@@ -260,7 +260,7 @@ if __name__ == "__main__":
         result_file_loss="./cifar/iid/loss",
     )
 
-    config = debug
+    config = grouping
 
     gfl = GFL(config)
     gfl.train()
